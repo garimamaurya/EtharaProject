9cf1b01691c0d44d149a826bf5da7588551ea7f0
Garima Maurya <garima2026@gmail.com>
2026-05-02 20:12:58 +0530

    fix env

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,4 @@
-console.log("🔥 THIS IS THE REAL SERVER FILE RUNNING");
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
